428a342656131476e7edbfa3fbde3f114ca2b8c5
minghui.shen <13030365596@163.com>
2022-05-20 14:02:14 +0800

    代码块标签

--- select/main.go
+++ select/main.go
@@ -17,6 +17,11 @@ func main() {
 				case job1 := <-ch1:
 					fmt.Println(job1)
 				default:
+					/**
+					 *break语句还可以在语句后面添加标签，
+					 *表示退出某个标签对应的代码块，
+					 *标签要求必须定义在对应的for、switch和 select的代码块上。
+					 */
 					break priority
 				}
 			}
