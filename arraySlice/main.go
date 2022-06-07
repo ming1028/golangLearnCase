afae8505d24aa7c3d9b13fb6d1e7dbd2b75ff73c
DESKTOP-NI0R99B\DELL <13030365596@163.com>
2022-06-07 16:27:55 +0800

    数组/slice string

---
+++ arraySlice/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr1 := new([5]int)
+	fmt.Println(arr1)
+	editArr(arr1)
+	fmt.Println(arr1)
+
+	arr2 := [...]int{4, 4, 5}
+	fmt.Println(arr2)
+
+	sli1 := arr2[:len(arr2)]
+	fmt.Println(sli1)
+	sli1 = sli1[:len(arr2)-1]
+	fmt.Println(sli1)
+}
+
+func editArr(arr *[5]int) {
+	arr[1] = 66
+}
---
+++ string/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+	str := "hello"
+	for _, u := range str {
+		fmt.Print(u, " ")
+	}
+	fmt.Println()
+	// 修改string转为byte，然后修改
+	strByte := []byte(str)
+	fmt.Println(strByte)
+	for _, s := range strByte {
+		fmt.Print(string(s), " ")
+	}
+	fmt.Println()
+	// 截取
+	strSli := strByte[:3]
+	fmt.Println(string(strSli))
+	fmt.Println("字节数：", len(str), "字符数：", utf8.RuneCountInString(str))
+
+	// 字符串连接
+	b := bytes.NewBufferString(str)
+	b.WriteString(" ")
+	b.WriteString(" world")
+	fmt.Println(b.String())
+}
