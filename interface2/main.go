9c0a045811da10ff356e4d79304585b62e32a3ee
minghui.shen <13030365596@163.com>
2022-08-02 17:30:41 +0800

    接口实现

---
+++ interface2/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type People interface {
+	Speak(string) string
+}
+
+type Student struct{}
+
+// Speak 指针实现
+func (stu *Student) Speak(think string) (talk string) {
+	if think == "sb" {
+		talk = "sb"
+	} else {
+		talk = "您好"
+	}
+	return
+}
+
+func main() {
+	var peo People = &Student{} // 指针接受者实现了People接口，是People类型
+	think := "bitch"
+	fmt.Println(peo.Speak(think))
+}
