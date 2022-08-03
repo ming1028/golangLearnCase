ccbb4bd50881807e9f574dc502bd90de7aafdcf0
minghui.shen <13030365596@163.com>
2022-08-03 13:55:05 +0800

    work pool

---
+++ workPool/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+type Job struct {
+	Id      int32
+	RandNum int32
+}
+
+type Result struct {
+	job      *Job
+	sum      int32
+	goFunNum int32
+}
+
+func main() {
+	jobChan := make(chan *Job, 128)
+	resultChan := make(chan *Result, 128)
+	createPool(4, jobChan, resultChan)
+
+	go func(resultChan chan *Result) {
+		for result := range resultChan {
+			fmt.Printf("gofunNUm %v job id:%v randnum:%v result:%d\n", result.goFunNum, result.job.Id,
+				result.job.RandNum, result.sum)
+		}
+	}(resultChan)
+
+	for id := 0; ; id++ {
+		rNum := rand.Int31()
+		jobChan <- &Job{
+			Id:      int32(id),
+			RandNum: rNum,
+		}
+	}
+}
+
+func createPool(
+	num int,
+	jobChan chan *Job,
+	resultChan chan *Result,
+) {
+	for i := 0; i < num; i++ {
+		chanNum := i
+		go func(
+			jobChan chan *Job,
+			resultChan chan *Result,
+			chanNum int,
+		) {
+			for job := range jobChan {
+				rNum := job.RandNum
+				var sum int32
+				for rNum != 0 {
+					sum += rNum % 10
+					rNum /= 10
+				}
+				resultChan <- &Result{
+					job:      job,
+					sum:      sum,
+					goFunNum: int32(chanNum),
+				}
+			}
+		}(jobChan, resultChan, chanNum)
+	}
+}
