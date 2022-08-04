13f2f700c8e9e7f4e6982641dc43c080dd8174a3
minghui.shen <13030365596@163.com>
2022-08-04 10:03:15 +0800

    爬虫 正则匹配QQ号

---
+++ reptile/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+)
+
+var (
+	reEmail = `\w+@\w+\.\w+` // w代表大小写字母、数字、下划线
+	// s?有或者没有s
+	// +代表出1次或多次
+	// \s\S各种字符
+	// +?代表贪婪模式
+	reLinke = `href="(https?://[\s\S]+?)"`
+)
+
+func main() {
+	resp, err := http.Get("https://tieba.baidu.com/p/6051076813?red_tag=1573533731")
+	if err != nil {
+		log.Fatalf("http get err:%#v\n", err)
+	}
+	defer resp.Body.Close()
+
+	pageBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("ioutil ReadAll err:%#v\n", err)
+	}
+
+	rege := regexp.MustCompile(`(\d+)@qq.com`) // 传入正则表达式，得到正则表达式的struct
+	res := rege.FindAllStringSubmatch(string(pageBytes), -1)
+	fmt.Println(res)
+	return
+	for _, qq := range res {
+		fmt.Println(qq)
+		fmt.Println("email:", qq[0])
+		fmt.Println("qq:", qq[1])
+	}
+}
