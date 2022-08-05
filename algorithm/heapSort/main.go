484152fe12055f18c94d52a2db5e572b5d2073a8
minghui.shen <13030365596@163.com>
2022-08-05 15:56:54 +0800

    堆排序

--- algorithm/heapSort/main.go
+++ algorithm/heapSort/main.go
@@ -15,9 +15,9 @@ func HeapSort(arr []int32) []int32 {
 	for i := 0; i < length; i++ {
 		lastlen := length - i
 		HeapSortMax(arr, int32(lastlen))
-		if i < length {
+		/*if i < length {
 			arr[0], arr[lastlen-1] = arr[lastlen-1], arr[0]
-		}
+		}*/
 	}
 	return arr
 }
@@ -27,18 +27,23 @@ func HeapSortMax(arr []int32, length int32) {
 		return
 	}
 	depth := length/2 - 1 //二叉树深度
+	fmt.Println(1, arr)
 	for i := depth; i >= 0; i-- {
 		topmax := i //假定最大的位置就在i的位置
 		leftchild := 2*i + 1
 		rightchild := 2*i + 2
+		fmt.Println(topmax, leftchild, rightchild)
 		if leftchild <= length-1 && arr[leftchild] > arr[topmax] { //防止越过界限
 			topmax = leftchild
 		}
 		if rightchild <= length-1 && arr[rightchild] > arr[topmax] { //防止越过界限
 			topmax = rightchild
 		}
+		fmt.Println(topmax)
 		if topmax != i {
 			arr[i], arr[topmax] = arr[topmax], arr[i]
+			fmt.Println(2, arr)
 		}
 	}
+	fmt.Println(3, arr)
 }
