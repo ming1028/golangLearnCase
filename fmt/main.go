5996888f0018226c8f2a217cb011abf74cc15b2f
minghui.shen <13030365596@163.com>
2022-08-05 09:46:45 +0800

    fmt

---
+++ fmt/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	/*var (
+		name    string
+		age     int32
+		married bool
+	)*/
+	// 从标准输入扫描文本，读取由空白符分隔的值
+	/*fmt.Scan(&name, &age, &married) // 强制类型转换
+	 */
+	// fmt.Scanf("1:%s 2:%d 3:%t", &name, &age, &married)
+	// fmt.Scanln(&name, &age, &married)
+	// fmt.Printf("扫描结果 name:%s age:%d married:%t \n", name, age, married)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("请输入内容")
+	text, err := reader.ReadString('\n') // 读到换行
+	if err != nil {
+		fmt.Errorf("read from stdin err:%v", err)
+		return
+	}
+	text = strings.TrimSpace(text)
+	fmt.Printf("%#v\n", text)
+}
