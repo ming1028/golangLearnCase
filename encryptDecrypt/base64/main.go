585759064aa6cd6485bf258aedad8e56c2bae8c4
minghui.shen <13030365596@163.com>
2022-08-09 14:48:40 +0800

    base64 StdEncoding URLEncoding

---
+++ encryptDecrypt/base64/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+)
+
+func main() {
+	str := "123"
+	input := []byte(str)
+	fmt.Printf("[]byte : %v\n", input)
+
+	encodeString := base64.StdEncoding.EncodeToString(input)
+	fmt.Printf("encode base64 : %v\n", encodeString)
+
+	decodeBytes, _ := base64.StdEncoding.DecodeString(encodeString)
+	fmt.Printf("decode base64: %v\n", string(decodeBytes))
+
+	input = []byte("34234dfsdfsf")
+	urlEncode := base64.URLEncoding.EncodeToString(input)
+	fmt.Printf("urlencode:%v\n", urlEncode)
+
+	urlDecode, _ := base64.URLEncoding.DecodeString(urlEncode)
+	fmt.Printf("urldecode:%v\n", string(urlDecode))
+}
