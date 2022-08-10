ed1bc658db6cdebaacd67d821c42438361ab34be
minghui.shen <13030365596@163.com>
2022-08-10 09:13:52 +0800

    hmac加密

---
+++ encryptDecrypt/hmac/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	hmac2 "crypto/hmac"
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+)
+
+func main() {
+	key := "hello world"
+	data := "234242hello"
+	hmac := hmac2.New(md5.New, []byte(key))
+	hmac.Write([]byte(data))
+	fmt.Println(hex.EncodeToString(hmac.Sum([]byte(""))))
+}
