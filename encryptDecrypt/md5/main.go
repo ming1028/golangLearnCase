876c8e2af85fd17bc9525431986eb9fb63d6c812
minghui.shen <13030365596@163.com>
2022-08-10 09:33:35 +0800

    md5备注

--- encryptDecrypt/md5/main.go
+++ encryptDecrypt/md5/main.go
@@ -4,18 +4,18 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"fmt"
-	"io"
 )
 
 func main() {
 	str := "md5加密"
 	data := []byte(str)
 	has := md5.Sum(data)
-	md5Str1 := fmt.Sprintf("%x", has)
+	md5Str1 := fmt.Sprintf("%x", has) // 转换成16进制
 	fmt.Println(md5Str1)
 
 	w := md5.New()
-	io.WriteString(w, str)
+	//io.WriteString(w, str) // 将str写入到w中 方式1
+	w.Write(data) // 方式2
 	bw := w.Sum(nil)
 
 	md5str2 := hex.EncodeToString(bw)
