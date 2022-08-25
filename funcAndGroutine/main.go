cfc44950f7f126ebd5bf72b8e3c83fecb8b5b7af
minghui.shen <13030365596@163.com>
2022-08-25 14:54:28 +0800

    闭包参数

--- funcAndGroutine/main.go
+++ funcAndGroutine/main.go
@@ -8,7 +8,7 @@ import (
 var values = [5]int{1, 2, 3, 4, 5} // 常量不能用 :=
 
 func main() {
-	fmt.Print("闭包：")
+	fmt.Print("闭包 顺序执行：")
 	for _, value := range values {
 		func() {
 			fmt.Print(value, " ")
@@ -34,11 +34,13 @@ func main() {
 	}
 	time.Sleep(5e9)
 
-	fmt.Print("\n协程变量赋值：")
+	fmt.Print("\n协程变量赋值：\n")
 	for _, value := range values {
 		val := value // 变量声明是在循环体内部，所以在每次循环时，这些变量相互之间是不共享的，所以这些变量可以单独的被每个闭包使用。
+		fmt.Printf("变量地址：%p\n", &val)
 		go func() {
 			fmt.Print(val, " ")
+			fmt.Printf("%p\n", &val)
 		}()
 	}
 	time.Sleep(5e9)
