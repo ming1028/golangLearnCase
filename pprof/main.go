48d582db5d2563b3b0771551214ee8e91bed6527
minghui.shen <13030365596@163.com>
2022-08-29 15:02:35 +0800

    pprof

--- pprof/main.go
+++ pprof/main.go
@@ -43,3 +43,10 @@ func main() {
 	//启动pprof
 	http.ListenAndServe("0.0.0.0:10000", nil)
 }
+
+// 访问localhost:10000/debug/pprof 点击profile生成一个profile文件  使用go tool pprof profile文件路径
+// 输入web 生成调用文件图片
+
+// Mac： brew install graphviz
+
+// Windows: 下载graphviz 将graphviz安装目录下的bin文件夹添加到Path环境变量中。 在终端输入dot -version查看是否安装成功
