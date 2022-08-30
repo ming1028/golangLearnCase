2211de7243d45d817380d2fdaab4cdccdd49d2b6
minghui.shen <13030365596@163.com>
2022-08-30 16:07:48 +0800

    slice排序

--- slice_unique/main.go
+++ slice_unique/main.go
@@ -7,8 +7,12 @@ import (
 
 func main() {
 	sli := []int{12, 3, 65, 12}
-	sort.SliceStable(sli, func(i, j int) bool {
+	sort.SliceStable(sli, func(i, j int) bool { // 严格模式 保持原有顺序
 		return sli[i] < sli[j]
+	})
+	fmt.Println(sli)
+	sort.Slice(sli, func(i, j int) bool {
+		return sli[i] > sli[j]
 	})
 	fmt.Println(sli)
 }
