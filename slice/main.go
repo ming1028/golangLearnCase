0dd45a43f896c5fada62e9c66bde019f4ff6de69
minghui.shen <13030365596@163.com>
2022-08-30 14:30:07 +0800

    slice copy

--- slice/main.go
+++ slice/main.go
@@ -14,8 +14,8 @@ func main() {
 
 	sli1 = []int{1, 2, 3, 4, 5, 6}
 	sli2 = make([]int, 12)
-	n := copy(sli1, sli2)
-	fmt.Println(n, sli2)
+	n := copy(sli1, sli2) // sli2 => sli1
+	fmt.Println(n, sli2, sli1)
 	sli3 := []int{1, 2, 3, 4, 5, 6, 7, 8}
 	sli4 := make([]int, 6)
 	n = copy(sli4, sli3)
