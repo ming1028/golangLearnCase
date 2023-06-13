89d5614ec9cf2f3a6d7683d65f766080215d6b78
minghui.shen <shenminghui@huashengfe.com>
2023-06-13 14:21:59 +0800

    杂记

--- interface2/main.go
+++ interface2/main.go
@@ -26,8 +26,26 @@ func main() {
 	var peo People = &Student{} // 指针接受者实现了People接口，是People类型
 	think := "bitch"
 	fmt.Println(peo.Speak(think))
+	m := make(map[string]*student)
+	stus := []student{
+		{name: "pprof.cn", age: 18},
+		{name: "测试", age: 23},
+		{name: "博客", age: 28},
+	}
+
+	for _, stu := range stus {
+		m[stu.name] = &stu
+	}
+	for k, v := range m {
+		fmt.Println(k, "=>", v.name)
+	}
 }
 
 func (stu *Student) say() {
 	fmt.Println(123)
+}
+
+type student struct {
+	name string
+	age  int
 }
