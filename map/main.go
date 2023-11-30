a5422b9633ee65cdd2d0a62fa67997bd508899b0
minghui.shen <shenminghui@huashengfe.com>
2023-11-30 14:45:06 +0800

    map

--- map/main.go
+++ map/main.go
@@ -1,15 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
+	fmt.Println(fmt.Sprintf("  %x", 455))
 	m1 := map[string]int{
 		"a": 1,
 		"b": 2,
 		"c": 3,
 		"d": 4,
 	}
-	m2 := make(map[string]int)
+	// mj, _ := json.Marshal(m1)
+	for i := 0; i <= 20; i++ {
+		j := i
+		/*m22 := make(map[string]int)
+		json.Unmarshal(mj, m22)*/
+		go func() {
+			time.Sleep(time.Second)
+			m1["a"] = j
+			fmt.Println(m1["a"])
+		}()
+	}
+	time.Sleep(time.Second * 15)
+	fmt.Println(m1)
+	/*m2 := make(map[string]int)
 	m2 = m1
 	m2["a"] = 333
 	fmt.Println(m1, m2)
@@ -19,5 +36,5 @@ func main() {
 	for v := range m1 {
 		fmt.Println(v)
 	}
-	fmt.Println(m1)
+	fmt.Println(m1)*/
 }
