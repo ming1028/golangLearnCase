63d3f2e4792852ff106d5b31b193c7e959e46510
minghui.shen <shenminghui@huashengfe.com>
2023-12-07 11:41:28 +0800

    gorm

--- goOrm/main.go
+++ goOrm/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"github.com/fatih/structs"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"log"
 	"time"
 )
 
@@ -27,20 +27,24 @@ func main() {
 	db, _ := gorm.Open("mysql", "root:root@(localhost)/gorm?charset=utf8mb4&parseTime=True&loc=Local")
 	defer db.Close()
 
-	db.SingularTable(false) // 不要复数表名
+	db.LogMode(true)
+	// db.Callback().Query().After("gorm:query").Register("my_plugin:after_query", afterQueryCallBack)
+	/*db.SingularTable(false) // 不要复数表名
 	db.AutoMigrate(&UserInfo22{})
 
 	u1 := UserInfo22{Name: "七米", Age: sql.NullInt64{Int64: 20, Valid: true}, Email: "篮球"}
 	u2 := UserInfo22{Name: "七米2", Age: sql.NullInt64{Int64: 20, Valid: true}, Email: "篮球"}
-
-	// 创建记录
+	*/
+	/*// 创建记录
 	db.Create(&u1)
-	db.Create(&u2)
+	db.Create(&u2)*/
 
 	// 查询
 	var u = new(UserInfo22)
-	db.First(u)
-	fmt.Println("=============")
+	/*err := db.Model(u).Where("age = ?", 23).Unscoped().Scan(u).Error
+	fmt.Println(err, u.Age)*/
+	db.Unscoped().First(u)
+	/*fmt.Println("=============")
 	fmt.Printf("%#v\n", u)
 	user1Map4 := structs.Map(u)
 	for k, v := range user1Map4 {
@@ -52,7 +56,39 @@ func main() {
 	fmt.Printf("%#v\n", uu)
 
 	// 更新
-	db.Model(&u).Update("Email", "篮球")
+	db.Model(&u).Update("Email", "篮球")*/
 	// 删除
-	db.Delete(&u)
+	// db.Delete(&u)
+	/*us := make([]UserInfo22, 0, 8)
+	db.Find(&us).Where("age > ?", 1)*/
+
+	var ages []uint64
+	db.Model(&UserInfo22{}).Unscoped().Pluck("age", &ages)
+	fmt.Println(ages)
+
+	var us []UserInfo22
+	db.Table("user_info22").Unscoped().Where(map[string]interface{}{
+		"age": 23,
+	}).Find(&us)
+	fmt.Println(us)
+
+	db.Where(&UserInfo22{Age: sql.NullInt64{
+		Int64: 23,
+		Valid: true,
+	}}).Unscoped().Find(&us)
+	fmt.Println(us)
+
+	db.Not("name = ?", "a").Unscoped().Find(&us)
+	fmt.Println(us)
+
+	var u2 = new(UserInfo22)
+	db.Set("gorm:query_option", "for update").Unscoped().First(u2)
+	fmt.Println(u)
+
+	db.Unscoped().First(u2)
+	fmt.Println(u2)
+}
+
+func afterQueryCallBack(scope *gorm.Scope) {
+	log.Printf("raw sql:%v vars:%v", scope.SQL, scope.SQLVars)
 }
