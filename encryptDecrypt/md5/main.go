2d15ca61b9f664d5eacb29e3aca60ca921781aeb
minghui.shen <shenminghui@huashengfe.com>
2024-01-11 14:02:03 +0800

    md5

--- encryptDecrypt/md5/main.go
+++ encryptDecrypt/md5/main.go
@@ -15,11 +15,12 @@ func main() {
 	fmt.Println(md5Str1)
 
 	w := md5.New()
-	//io.WriteString(w, str) // 将str写入到w中 方式1
+	// io.WriteString(w, str) // 将str写入到w中 方式1
 	w.Write(data)
 	// 方式2
 	bw := w.Sum(nil) // 是否增加特定前缀
 	fmt.Println(bw)
+	// hex.EncodeToString 转换成十六进制的
 	md5str2 := hex.EncodeToString(bw)
 	fmt.Println(md5str2)
 }
