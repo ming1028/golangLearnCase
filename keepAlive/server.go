e4847f6f509e4e7140a51f1624524da5d1771569
minghui.shen <shenminghui@huashengfe.com>
2024-01-12 11:22:06 +0800

    备注

--- keepAlive/server.go
+++ keepAlive/server.go
@@ -12,7 +12,7 @@ func main() {
 		Handler: http.HandlerFunc(Index),
 	}
 	// 如果server的Handler为nil,将使用DefaultServeMux(全局变量)作为handler
-	http.HandleFunc("/index", Index)
+	http.HandleFunc("/index", Index) // 使用默认DefaultServeMux
 	// 方式2:使用http.NewServeMux 多路复用器中的Handle或者HandleFunc定义各种路由，赋值给server.Handler
 	server.ListenAndServe()
 }
