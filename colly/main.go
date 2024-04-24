03aad47f01f77079e537e3754200fc722713226b
minghui.shen <m13030365596@163.com>
2024-04-24 17:36:27 +0800

    杂记

--- colly/main.go
+++ colly/main.go
@@ -6,29 +6,28 @@ import (
 )
 
 func main() {
-	collector := colly.NewCollector()
+
+	collector := colly.NewCollector(
+		colly.AllowedDomains("https://www.collinsdictionary.com/"),
+	)
 	// 在请求之前调用
 	collector.OnRequest(func(request *colly.Request) {
+		fmt.Println("123")
 		fmt.Println(request.URL)
 	})
 	// 错误
 	collector.OnError(func(response *colly.Response, err error) {
+		fmt.Println("123")
 		fmt.Println(err, response.StatusCode)
 	})
-	// 收到响应标头后调用
-	collector.OnResponseHeaders(func(response *colly.Response) {
-		fmt.Println(response.Headers)
-	})
-	// 收到回复后调用
 	collector.OnResponse(func(response *colly.Response) {
-		fmt.Println(response.Request.URL)
+		fmt.Println("123")
+		fmt.Println(string(response.Body))
 	})
-	// 如果收到的内容是HTML ,则在之后调用
-	collector.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		e.Request.Visit(e.Attr("href"))
-	})
-	collector.OnHTML("tr td:nth-of-type(1)", func(e *colly.HTMLElement) {
-		fmt.Println("First column of a table row:", e.Text)
+	collector.OnHTML(".sense", func(e *colly.HTMLElement) {
+		fmt.Println(e.Text)
+		definition := e.ChildText("div.definition")
+		fmt.Println("Definition:", definition)
 	})
 
 	// 接收到的内容是XML ,则在之后调用
@@ -40,4 +39,6 @@ func main() {
 	collector.OnScraped(func(r *colly.Response) {
 		fmt.Println("Finished", r.Request.URL)
 	})
+	err := collector.Visit("https://baidu.com")
+	fmt.Println(err)
 }
