b5aaea07d0114a8e571e351ffebeed694e813575
minghui.shen <m13030365596@163.com>
2024-05-24 11:34:42 +0800

    杂记

--- nlp/main.go
+++ nlp/main.go
@@ -10,13 +10,13 @@ import (
 func main() {
 	// assuming the Stanford CoreNLP is running at http://localhost:9000
 	// create a new HttpClient instance
-	cmd := client.NewHttpClient([]string{"tokenize", "ssplit", "pos", "lemma", "parse", "depparse"}, "http://localhost:9000")
+	cmd := client.NewHttpClient([]string{"tokenize", "ssplit", "pos", "lemma", "parse", "depparse"}, "http://172.18.245.254:9000")
 
 	// a reference to the nlp Document
 	pb := &nlp.Document{}
 
 	// run NLP and receive data in pb
-	err := cmd.RunText(context.Background(), []byte(`i'm a student`), pb)
+	err := cmd.RunText(context.Background(), []byte(`Those who fall out of favour with the Party become "unpersons", disappearing with all evidence of their existence destroyed.lloplo0lpoloploplplplpol`), pb)
 	if err != nil {
 		panic(err)
 	}
