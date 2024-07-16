6304900c9c7d89554b50a7f9ee5219502d52b1fd
minghui.shen <m13030365596@163.com>
2024-07-16 12:10:59 +0800

    版本修改

--- grpc_etcd/client/etcd_zrpc_client.go
+++ grpc_etcd/client/etcd_zrpc_client.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"github.com/golangLearnCase/grpc_etcd/server/proto/search"
 	"github.com/zeromicro/go-zero/core/discov"
 	"github.com/zeromicro/go-zero/zrpc"
 )
@@ -8,8 +11,17 @@ import (
 func main() {
 	conn := zrpc.MustNewClient(zrpc.RpcClientConf{
 		Etcd: discov.EtcdConf{ // 通过 etcd 服务发现时，只需要给 Etcd 配置即可
-			Hosts: []string{"127.0.0.1:2379"},
+			Hosts: []string{"127.0.0.1:2388"},
 			Key:   "greet.rpc",
 		},
 	})
+
+	searchClient := search.NewSearchServiceClient(conn.Conn())
+	resp, err := searchClient.Search(context.Background(), &search.SearchReq{
+		Name: "zrpc search:",
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(resp.GetRespName())
 }
